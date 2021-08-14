f2c0b12e838a752aa7b4e10edbe596046bb92b68
jojeh <jo.jehyeon0403@gmail.com>
2021-08-15 03:07:26 +0900

    프로젝트 생성

---
+++ src/main/java/exer/exerboard/ExerBoardApplication.java
@@ -0,0 +1,13 @@
+package exer.exerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exer/exerboard/ExerBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package exer.exerboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
